f15bdd45782e8765ac12d25cc1a5a5b09fa085c4
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-12-16 18:20:40 -0200

    Assets are automaticaly inserted into the index.html

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+const html = require('html-webpack-plugin');
 
 // const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
@@ -26,23 +26,14 @@ module.exports = merge(common, {
 					]
 				})
 			},
-				// use: ExtractTextPlugin.extract({
-				// 	use: [
-				// 	// {
-				// 	// 	loader: "css-loader"
-				// 	// }, 
-				// 		{
-				// 			loader: "sass-loader"
-				// 		}
-				// 	],
-				// 	// use style-loader in development
-				// 	// fallback: "style-loader"
-				// })
 		]
 	},
 
 	plugins: [
 		new ExtractTextPlugin("styles.css"),
+		new html({
+			template : './src/index.html'
+		})
 	// new UglifyJSPlugin()
 	]
 });
